a3f0f5649e2c826f1d405b5ba0aed9da225be7b7
nickvac642 <nickwvac@gmail.com>
2019-03-08 13:47:04 -0500

    Progress made on understanding functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors())
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/users', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({users: ["nick", "owen", "saucetrey"]}));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
